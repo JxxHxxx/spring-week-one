73784a371ea5a52c8491935f373c4809f3b96e9a
JxxHxxx <leesin5498@naver.com>
2022-12-25 00:44:05 +0900

    첫 커밋

---
+++ src/main/java/com/sparta/springweekone/SpringWeekOneApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springweekone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeekOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeekOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springweekone/SpringWeekOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springweekone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeekOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
