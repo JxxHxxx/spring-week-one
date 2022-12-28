077c11d3d4677011bce281ff97a1feb68a0146f4
JxxHxxx <leesin5498@naver.com>
2022-12-28 23:06:26 +0900

    RFTR #3 서비스 단에는 서비스 로직만!

--- src/main/java/com/sparta/springweekone/bulletinboard/domain/BulletinBoardService.java
+++ src/main/java/com/sparta/springweekone/bulletinboard/domain/BulletinBoardService.java
@@ -3,14 +3,10 @@ import com.sparta.springweekone.bulletinboard.dto.*;
 import com.sparta.springweekone.bulletinboard.repository.BulletinBoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +16,14 @@ import java.util.stream.Collectors;
 public class BulletinBoardService {
 
     private final BulletinBoardRepository bulletinBoardRepository;
-//    private final PasswordEncoder passwordEncoder;
-
 
     public BulletinBoardDto create(BulletinBoardForm boardForm) {
-//        String encryptedPassword = passwordEncoder.encrypt(boardForm.getPassword());
-
         BulletinBoard board = new BulletinBoard(boardForm, boardForm.getPassword());
         BulletinBoard saveBoard = bulletinBoardRepository.save(board);
 
         return new BulletinBoardDto(saveBoard);
-
     }
+
     public List<BulletinBoardDto> readAll() {
         List<BulletinBoard> boards = bulletinBoardRepository.findAllByOrderByCreateAtDesc();
 
@@ -42,53 +34,35 @@ public class BulletinBoardService {
         BulletinBoard board = bulletinBoardRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 게시글입니다.")
         );
-        BulletinBoardDto boardDto = new BulletinBoardDto(board);
 
-        return boardDto;
+        return new BulletinBoardDto(board);
     }
+
     public ResultDto delete(Long id, PasswordDto passwordDto) {
         BulletinBoard board = bulletinBoardRepository.findById(id).orElseThrow();
 
         if (isNotSame(passwordDto.getPassword(), board.getPassword())) {
             return new ResultDto(false);
         }
+
         bulletinBoardRepository.deleteById(id);
         return new ResultDto(true);
-
-    }
-
-    @Deprecated
-    @Transactional
-    public BulletinBoardDto update(Long id, BulletinBoardForm boardForm) {
-        BulletinBoard board = bulletinBoardRepository.findById(id).orElseThrow();
-        if (isNotSame(boardForm.getPassword(), board.getPassword())) {
-            return null;
-        }
-
-        board.update(boardForm);
-        return new BulletinBoardDto(board);
     }
 
     @Transactional
-    public ResponseEntity<Message> updateV2(Long id, BulletinBoardForm boardForm) {
+    public Message update(Long id, BulletinBoardForm boardForm) {
         BulletinBoard board = bulletinBoardRepository.findById(id).orElseThrow();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-
         if (isNotSame(boardForm.getPassword(), board.getPassword())) {
-            Message message = new Message(false, null);
-            return new ResponseEntity<>(message, headers, HttpStatus.UNAUTHORIZED);
+            return new Message(false, null);
         }
 
         board.update(boardForm);
 
-        Message message = new Message(true, new BulletinBoardDto(board));
-        return new ResponseEntity<>(message, headers, HttpStatus.OK);
+        return new Message(true, new BulletinBoardDto(board));
     }
 
     private boolean isNotSame(String passwordOfDto, String passwordOfEntity) {
-//        String encryptedPassword = passwordEncoder.encrypt(passwordOfDto);
         return !passwordOfEntity.equals(passwordOfDto);
     }
 
