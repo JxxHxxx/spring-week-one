d23285c4a4cadbd2973f8d61e297967e0c6c89ef
JxxHxxx <leesin5498@naver.com>
2022-12-28 22:37:48 +0900

    RFTR #3 컨트롤러 리팩토링
    url prefix 삭제
    url 단수 -> 복수
    사용하지 않는 API 삭제
    생성자 -> 롬복 어노테이션 대체

--- src/main/java/com/sparta/springweekone/bulletinboard/controller/BulletinBoardController.java
+++ src/main/java/com/sparta/springweekone/bulletinboard/controller/BulletinBoardController.java
@@ -2,6 +2,7 @@ package com.sparta.springweekone.bulletinboard.controller;
 
 import com.sparta.springweekone.bulletinboard.domain.BulletinBoardService;
 import com.sparta.springweekone.bulletinboard.dto.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +11,13 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api")
+@RequiredArgsConstructor
 public class BulletinBoardController {
 
     private final BulletinBoardService bulletinBoardService;
 
-    public BulletinBoardController(BulletinBoardService bulletinBoardService) {
-        this.bulletinBoardService = bulletinBoardService;
-    }
-
     // 게시글 작성
-    @PostMapping("/bulletin-board")
+    @PostMapping("/bulletin-boards")
     public BulletinBoardDto write(@RequestBody BulletinBoardForm boardForm) {
         return bulletinBoardService.create(boardForm);
     }
@@ -31,25 +28,19 @@ public class BulletinBoardController {
         return bulletinBoardService.readAll();
     }
     // 선택 게시글 조회
-    @GetMapping("/bulletin-board/{id}")
+    @GetMapping("/bulletin-boards/{id}")
     public BulletinBoardDto readOne(@PathVariable Long id) {
         return bulletinBoardService.readOne(id);
     }
 
     // 선택 게시글 수정
-    @Deprecated
-    @PatchMapping("/bulletin-board/{id}")
-    public BulletinBoardDto update(@PathVariable Long id, @RequestBody BulletinBoardForm boardForm) {
-        return bulletinBoardService.update(id, boardForm);
-    }
-    // 선택 게시글 수정 버전2
-    @PatchMapping("/bulletin-board/v2/{id}")
+    @PatchMapping("/bulletin-boards/{id}")
     public ResponseEntity<Message> updateV2(@PathVariable Long id, @RequestBody BulletinBoardForm boardForm) {
         return bulletinBoardService.updateV2(id, boardForm);
     }
 
     // 선택 게시글 삭제
-    @DeleteMapping("/bulletin-board/{id}")
+    @DeleteMapping("/bulletin-boards/{id}")
     public ResultDto remove(@PathVariable Long id, @RequestBody PasswordDto passwordDto) {
         return bulletinBoardService.delete(id, passwordDto);
     }
